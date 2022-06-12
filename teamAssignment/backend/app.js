d061761dd71b7a0a040e38297006a58ab922778b
Lusyluh <mav19003@byui.edu>
2022-06-12 07:37:55 +0200

    team assignment updated

--- teamAssignment/backend/app.js
+++ teamAssignment/backend/app.js
@@ -13,10 +13,25 @@ const app = express();
 app.use('/', require('./routes'))
 
 // Configuring body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 
+  app.use(bodyParser.json())
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  app.use('/professional', professionalRoutes);
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
 app.listen(port, () => {
     console.log(`REST API app running on port ${port}`)
 })
