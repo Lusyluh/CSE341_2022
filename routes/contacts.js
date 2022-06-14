36e3312694860f2b8d40ce4dca1521fb49f4e88a
Lusyluh <mav19003@byui.edu>
2022-06-14 23:05:55 +0200

    pa2 updated

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,5 +2,6 @@ const routes = require('express').Router();
 const contactsController = require('../controllers/');
 
 routes.get('/', contactsController.getDocuments);
+routes.get('/:id', contactsController.getSingle);
 
 module.exports = routes;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router();
 const name = require('../controllers/');
-
 routes.get('/', name.displayName);
+
+routes.use('/', require('./contacts'));
 
 module.exports = routes;
