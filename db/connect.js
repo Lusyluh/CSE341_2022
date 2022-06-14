36ffde83b28f8f76e18aa5542418462d9effd625
Lusyluh <mav19003@byui.edu>
2022-06-14 04:10:24 +0200

    connection to mongodb

---
+++ db/connect.js
@@ -0,0 +1,16 @@
+const {MongoClient} = require('mongodb');
+
+async function main(){
+    const uri = MONGODB_URI;
+    const client = new MongoClient(uri);
+
+    try{
+        await client.connect();
+
+    }catch (e){
+        console.error(e);
+    }finally{
+        await client.close();
+    }
+}
+main().catch(console.error);
