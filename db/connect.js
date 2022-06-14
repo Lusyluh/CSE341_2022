45cba7782f48679a8de55ee47b35219d3e526cae
Lusyluh <mav19003@byui.edu>
2022-06-14 04:22:12 +0200

    connect to mongodb

--- db/connect.js
+++ db/connect.js
@@ -1,16 +1,48 @@
-const {MongoClient} = require('mongodb');
+// const {MongoClient} = require('mongodb');
 
-async function main(){
-    const uri = MONGODB_URI;
-    const client = new MongoClient(uri);
+// async function main(){
+//     const uri = MONGODB_URI;
+//     const client = new MongoClient(uri);
 
-    try{
-        await client.connect();
+//     try{
+//         await client.connect();
 
-    }catch (e){
-        console.error(e);
-    }finally{
-        await client.close();
-    }
-}
-main().catch(console.error);
+//     }catch (e){
+//         console.error(e);
+//     }finally{
+//         await client.close();
+//     }
+// }
+// main().catch(console.error);
+const dotenv = require('dotenv');
+dotenv.config();
+const MongoClient = require('mongodb').MongoClient;
+
+let _db;
+
+const initDb = (callback) => {
+  if (_db) {
+    console.log('Db is already initialized!');
+    return callback(null, _db);
+  }
+  MongoClient.connect(process.env.MONGODB_URI)
+    .then((client) => {
+      _db = client;
+      callback(null, _db);
+    })
+    .catch((err) => {
+      callback(err);
+    });
+};
+
+const getDb = () => {
+  if (!_db) {
+    throw Error('Db not initialized');
+  }
+  return _db;
+};
+
+module.exports = {
+  initDb,
+  getDb,
+};
