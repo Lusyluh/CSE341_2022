d1f083312205bf780024717b855152a7681da41e
Lusyluh <mav19003@byui.edu>
2022-06-22 19:43:46 +0200

    updated router to add swagger

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const routes = require('express').Router();
 const name = require('../controllers/');
 routes.get('/', name.displayName);
 
+routes.use('/', require('./swagger'));
 routes.use('/contacts', require('./contacts'));
 
 module.exports = routes;
