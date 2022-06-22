c69c3da0e9cb8e7bb5478169912baa4113b98174
Lusyluh <mav19003@byui.edu>
2022-06-22 17:31:30 +0200

    swagger gui success

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
@@ -15,10 +20,10 @@ app
   .use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
-    if (err) {
-      console.log(err);
-    } else {
-      app.listen(port);
-      console.log(`Connected to DB and listening on ${port}`);
-    }
-  });
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
